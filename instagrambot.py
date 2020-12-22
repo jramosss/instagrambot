fad8378198ff14368ef7efeada4406951494b78d
jramosss <jramostod@gmail.com>
2020-12-22 17:15:46 -0300

    Scroll and like WIP

--- instagrambot.py
+++ instagrambot.py
@@ -3,48 +3,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 from credentials import username,password
 
 class InstagramBot:
-    #URL = 'https://www.instagram.com/juliramoss1/'
-    URL = 'https://www.instagram.com/p/CJCYrlojNjs/'
+    URL = 'https://www.instagram.com'
 
-    #ch_options = Options()
-    #ch_options.add_argument('--no-sandbox')
-    #ch_options.add_argument('--disable-dev-shm-usage')
+    ch_options = Options()
+    #ch_options.add_argument('--eager')
+    ch_options.page_load_strategy = 'eager'
 
     def __init__(self) -> None:
-        self.driver = webdriver.Chrome('chromedriver/chromedriver')
+        self.driver = webdriver.Chrome('chromedriver/chromedriver',options=self.ch_options)
 
-
+    
     def login (self):
         self.driver.get(url=self.URL)
 
-        sleep(2)
-
-        uname_btn = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[1]/div[1]/div/label/input')
-        uname_btn.click()
-        uname_btn.send_keys(username)
-        pwd_btn = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[1]/div[2]/div/label/input')
-        pwd_btn.click()
-        pwd_btn.send_keys(password)
-        login_btn = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[1]/div[3]/button/div')
-        login_btn.click()
-
-    
-    def login2 (self):
-        self.driver.get(url=self.URL)
-
         sleep(1)
 
-        login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div/div/div/div[3]/div[1]/a')
-        login_btn.click()
-
-        sleep(1)
         uname_btn = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
-        uname_btn.click()
+        #uname_btn.click()
         uname_btn.send_keys(username)
         pwd_btn = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
         pwd_btn.click()
@@ -78,8 +58,8 @@ class InstagramBot:
         res.click()
     
 
-    def troll_joacoto (self):
-        self.driver.get('https://www.instagram.com/joaco_torres1/')
+    def comment_fst_ph (self,message,url):
+        self.driver.get(url)
         sleep(1)
         #last_ph = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[4]/article/div[1]/div/div[1]/div[2]/a')
         last_ph = self.driver.find_element_by_class_name('_9AhH0')
@@ -88,10 +68,21 @@ class InstagramBot:
         comment_area = self.driver.find_element_by_class_name('Ypffh')
         comment_area.click()
         comment_area = self.driver.find_element_by_class_name('Ypffh')
-        comment_area.send_keys('Dragon blanco de ojos azules')
+        comment_area.send_keys(message)
         comment_area.send_keys(Keys.ENTER)
-        #send_btn = self.driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[3]/div/form/button')
-        #send_btn.click()
+
+
+    def scroll_and_like(self):
+        self.driver.get(self.URL)
+        sleep(1)
+        for _ in range (5000):
+            likes = self.driver.find_elements(By.CLASS_NAME,"fr66n")
+            for like in likes:
+                if like.get_attribute('height') != 16:
+                    like.click()
+
+            for _ in range (50):
+                webdriver.ActionChains(self.driver).key_down(Keys.ARROW_DOWN).perform()
 
 
 bot = InstagramBot()
@@ -102,13 +93,15 @@ browser.get(bot.URL)
 
 sleep(1)
 
-bot.login2()
+bot.login()
 
 sleep(0.5)
 
 #bot.go_to_profile()
-bot.go_to_someones_profile('joacoto')
+#bot.go_to_someones_profile('joacoto')
 
-sleep(1)
+#sleep(1)
 
-bot.troll_joacoto()
+#bot.comment_fst_ph('Dragon blanco de ojos azules',"https://www.instagram.com/joaco_torres1/")
+
+bot.scroll_and_like()
